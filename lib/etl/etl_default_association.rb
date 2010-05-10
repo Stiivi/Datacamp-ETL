500d5e0908a8306d6b5310d4e6a44ef88d6ac1e0
Stefan Urbanek <stefan@Stefan-Urbaneks-MacBook.local>
2010-05-11 00:32:10 +0200

    changed default value from string to text

--- lib/etl/etl_default_association.rb
+++ lib/etl/etl_default_association.rb
@@ -4,5 +4,5 @@ class ETLDefaultAssociation
 	property :id,            Serial
 	property :domain,        String
 	property :default_key,   String, :field => "key"
-	property :default_value, String, :field => "value"
+	property :default_value, Text, :field => "value"
 end
